dd8e35356fed7f744e7aa1c0727966e5d198ac4a
tate_Code <ashwintate111@gmail.com>
2026-05-19 11:23:33 +0530

    adding account files

---
+++ com/finace/account/accountType.java
@@ -0,0 +1,11 @@
+package com.finace.account;
+
+public class accountType {
+
+	public static void main(String[] args) {
+	
+		
+		System.out.println(" this is account opening ");
+	}
+
+}
--- test1.java
+++ test1.java
@@ -1,3 +1,8 @@
-class new {
-sysso();
+class test1{
+	
+	
+	public static void main(String args [])
+	{
+		
+		System.out.println("hii");	}
 }
