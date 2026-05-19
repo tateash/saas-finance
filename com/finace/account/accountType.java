631b475ce3cb0484be05a3c50ee3bfd7d5d2e78a
Ashwin <51111263+tateash@users.noreply.github.com>
2026-05-19 11:43:30 +0530

    Revert "creating account"

--- com/finace/account/accountType.java
+++ com/finace/account/accountType.java
@@ -2,20 +2,10 @@ package com.finace.account;
 
 public class accountType {
 
-	
-	 static public void  account()
-		{
-		 System.out.println("creating account ");
-		 
-			
-		}
 	public static void main(String[] args) {
 	
 		
 		System.out.println(" this is account opening ");
-		
-		account();
-	
 	}
 
 }
